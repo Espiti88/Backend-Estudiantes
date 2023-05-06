9f64688ebb22d065786388d016d263912dc41116
Carlos Andres Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-05-06 16:40:23 -0500

    ACorrige endpoint de creación

--- src/main/java/co/edu/unisabana/SIGA/Controller.java
+++ src/main/java/co/edu/unisabana/SIGA/Controller.java
@@ -8,18 +8,19 @@ import java.util.stream.Collectors;
 @RestController
 public class Controller {
     List<Estudiante> estudiantes;
+    int idRegistro = 1;
     public Controller (){
         this.estudiantes = new ArrayList<>();
         estudiantes.add(new Estudiante(1, "Carlos Andrés", 3, "Ingeniería"));
         estudiantes.add(new Estudiante(2, "Juan José", 3, "Medicina"));
-        estudiantes.add(new Estudiante(8, "Mateo", 3, "Medicina"));
-        estudiantes.add(new Estudiante(9, "Pavel", 3, "Comunicación"));
-        estudiantes.add(new Estudiante(10, "Turry", 3, "Comunicación"));
         estudiantes.add(new Estudiante(3, "Laura Sofía", 3, "EICEA"));
         estudiantes.add(new Estudiante(4, "Valentina", 3, "Ingeniería"));
         estudiantes.add(new Estudiante(5, "Manuela", 3, "Ingeniería"));
         estudiantes.add(new Estudiante(6, "Diana", 3, "Medicina"));
         estudiantes.add(new Estudiante(7, "María Fernanda", 3, "EICEA"));
+        estudiantes.add(new Estudiante(8, "Mateo", 3, "Medicina"));
+        estudiantes.add(new Estudiante(9, "Pavel", 3, "Comunicación"));
+        estudiantes.add(new Estudiante(10, "Turry", 3, "Comunicación"));
     }
 
     @GetMapping(path = "/")
@@ -42,13 +43,13 @@ public class Controller {
 
     @PostMapping(path = "/estudiantes/crear")
     public String crearEstudiante(@RequestBody Estudiante estudiante){
-        int newId = 1;
         for(Estudiante estudianteX: estudiantes){
-            if(estudianteX.getId() == newId){
-                newId += 1;
+            if(estudianteX.getId() >= idRegistro){
+                idRegistro += 1;
             }
         }
-        estudiante.setId(newId);
+        estudiante.setId(idRegistro);
+        idRegistro += 1;
         estudiantes.add(estudiante);
         return "¡Estudiante guardado!";
     }
