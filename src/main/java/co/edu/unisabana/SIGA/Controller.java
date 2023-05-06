da76f2c69b815fe189d7a0a2372c7818bd29ac21
Carlos Andres Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-05-06 15:56:43 -0500

    Agrega endpoint de creación de estudiantes

--- src/main/java/co/edu/unisabana/SIGA/Controller.java
+++ src/main/java/co/edu/unisabana/SIGA/Controller.java
@@ -12,7 +12,14 @@ public class Controller {
         this.estudiantes = new ArrayList<>();
         estudiantes.add(new Estudiante(1, "Carlos Andrés", 3, "Ingeniería"));
         estudiantes.add(new Estudiante(2, "Juan José", 3, "Medicina"));
+        estudiantes.add(new Estudiante(8, "Mateo", 3, "Medicina"));
+        estudiantes.add(new Estudiante(9, "Pavel", 3, "Comunicación"));
+        estudiantes.add(new Estudiante(10, "Turry", 3, "Comunicación"));
         estudiantes.add(new Estudiante(3, "Laura Sofía", 3, "EICEA"));
+        estudiantes.add(new Estudiante(4, "Valentina", 3, "Ingeniería"));
+        estudiantes.add(new Estudiante(5, "Manuela", 3, "Ingeniería"));
+        estudiantes.add(new Estudiante(6, "Diana", 3, "Medicina"));
+        estudiantes.add(new Estudiante(7, "María Fernanda", 3, "EICEA"));
     }
 
     @GetMapping(path = "/")
@@ -21,15 +28,28 @@ public class Controller {
     }
     //http://localhost:8080/
 
-    @GetMapping(path = "/estudiantes")
+    @GetMapping(path = "/estudiantes/todos")
     public List<Estudiante> allEstudiantes(){
         return estudiantes;
     }
     //http://localhost:8080/estudiantes
 
-    @RequestMapping(value = "/estudiantes", params = "facultad")
+    @RequestMapping(value = "/estudiantes/buscar", params = "facultad")
     public List<Estudiante> buscarFacultad(@RequestParam String facultad){
         return estudiantes.stream().filter((estudiante) -> estudiante.getFacultad().equals(facultad)).collect(Collectors.toList());
     }
     //http://localhost:8080/estudiantes?facultad=
+
+    @PostMapping(path = "/estudiantes/crear")
+    public String crearEstudiante(@RequestBody Estudiante estudiante){
+        int newId = 1;
+        for(Estudiante estudianteX: estudiantes){
+            if(estudianteX.getId() == newId){
+                newId += 1;
+            }
+        }
+        estudiante.setId(newId);
+        estudiantes.add(estudiante);
+        return "¡Estudiante guardado!";
+    }
 }
