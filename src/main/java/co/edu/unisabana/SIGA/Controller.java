88db41c1725b31e3dcf5bbf69e4a1b80ea060d33
Carlos Andres Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-05-06 12:11:28 -0500

    Agrega endpoint inicial, un estudiantes de prueba y endpoint para ver todos los estudiantes

--- src/main/java/co/edu/unisabana/SIGA/Controller.java
+++ src/main/java/co/edu/unisabana/SIGA/Controller.java
@@ -1,5 +1,25 @@
 package co.edu.unisabana.SIGA;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+@RestController
 public class Controller {
+    List<Estudiante> estudiantes;
+    public Controller (){
+        this.estudiantes = new ArrayList<>();
+        estudiantes.add(new Estudiante(1, "Carlos", 3, "Ingeniería"));
+    }
 
+    @GetMapping(path = "/")
+    public String saludar() {
+        return "Servidor de Estudiantes DANIEL SAAVEDRA";
+    }
+    //http://localhost:8080/
+
+    @GetMapping(path = "/estudiantes")
+    public List<Estudiante> allEstudiantes(){
+        return estudiantes;
+    }
+    //http://localhost:8080/estudiantes
 }
